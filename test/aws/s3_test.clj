7244a8c6951d4c7ffbd0adb476821e9de0abe315
nathants <me@nathants.com>
2015-07-19 23:55:54 -0700

    start on integration tests

--- test/aws/s3_test.clj
+++ test/aws/s3_test.clj
@@ -2,6 +2,16 @@
   (:require [clojure.test :refer :all]
             [aws.s3 :as s3]))
 
+(def *integration-test* false)
+
+(def creds
+  {:access-key (System/getenv "AWS_ACCESS_KEY")
+   :secret-key (System/getenv "AWS_SECRET_ACCESS_KEY")})
+
+(def bucket (System/getenv "S3_TEST_BUCKET"))
+
+(def prefix (System/getenv "S3_TEST_PREFIX"))
+
 (deftest test-prefixes
   (is (= ["a" "a/b" "a/b/c"]
          (s3/-prefixes "a/b/c/d.csv"))))
@@ -21,3 +31,32 @@
     (s3/with-stubbed-s3 data
       (is (= [key] (s3/list-keys nil bucket "a/fake/prefix")))
       (is (= content (s3/get-key-text nil bucket key))))))
+
+(when (and *integration-test* creds bucket prefix)
+  (defn temp-path
+    []
+    (.getAbsolutePath (java.io.File/createTempFile "temp" "")))
+
+  (defn uuid
+    []
+    (str (java.util.UUID/randomUUID)))
+
+  (deftest test-delete-get-put-get-delete-text
+    (let [key (str prefix "/" (uuid))
+          val (uuid)]
+      (s3/delete-key creds bucket key)
+      (is (thrown? Exception (s3/get-key-text creds bucket key)))
+      (s3/put-key-text creds bucket key val)
+      (is (= val (s3/get-key-text creds bucket key)))
+      (s3/delete-key creds bucket key)))
+
+  (deftest test-delete-get-put-get-delete-path
+    (let [key (str prefix "/" (uuid))
+          val (uuid)
+          path (temp-path)]
+      (s3/delete-key creds bucket key)
+      (is (thrown? Exception (s3/get-key-text creds bucket key)))
+      (spit path val)
+      (s3/put-key-path creds bucket key path)
+      (is (= val (s3/get-key-text creds bucket key)))
+      (s3/delete-key creds bucket key))))
