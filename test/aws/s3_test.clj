3a8b77ec5540c3675d293ae042ede650401015c5
nathants <me@nathants.com>
2015-07-20 21:36:21 -0700

    integration tests

--- test/aws/s3_test.clj
+++ test/aws/s3_test.clj
@@ -1,8 +1,11 @@
 (ns aws.s3-test
+  (:import com.amazonaws.services.s3.model.DeleteObjectsRequest$KeyVersion)
   (:require [clojure.test :refer :all]
+            [amazonica.aws.s3 :as amz.s3]
+            [clojure.string :as s]
             [aws.s3 :as s3]))
 
-(def *integration-test* false)
+(def *integration-test* true)
 
 (def creds
   {:access-key (System/getenv "AWS_ACCESS_KEY")
@@ -10,7 +13,7 @@
 
 (def bucket (System/getenv "S3_TEST_BUCKET"))
 
-(def prefix (System/getenv "S3_TEST_PREFIX"))
+(def prefix "testing/cljs_aws")
 
 (deftest test-prefixes
   (is (= ["a" "a/b" "a/b/c"]
@@ -32,7 +35,33 @@
       (is (= [key] (s3/list-keys nil bucket "a/fake/prefix")))
       (is (= content (s3/get-key-text nil bucket key))))))
 
-(when (and *integration-test* creds bucket prefix)
+  (defn wipe-test-keys
+    []
+    (as-> {:bucket-name bucket :prefix prefix :max-keys 1000000} $
+          (amz.s3/list-objects creds $)
+          (:object-summaries $)
+          (map :key $)
+          (doto $
+            (->> (mapv #(println "deleting test s3 key:" %))))
+          (if (-> $ count (> 1))
+            (amz.s3/delete-objects creds :bucket-name bucket :keys (mapv #(DeleteObjectsRequest$KeyVersion. %) $))
+            (mapv #(amz.s3/delete-object creds bucket %) $))))
+
+  (defmacro with-wipe-test-keys
+    [& forms]
+    `(do (wipe-test-keys)
+         (let [res# (do ~@forms)]
+           (wipe-test-keys)
+           res#)))
+
+(when (and *integration-test* creds bucket)
+
+  (defn mk-key
+    [key]
+    (str (s/replace prefix #"/$" "")
+         "/"
+         (s/replace key #"^/" "")))
+
   (defn temp-path
     []
     (.getAbsolutePath (java.io.File/createTempFile "temp" "")))
@@ -41,22 +70,48 @@
     []
     (str (java.util.UUID/randomUUID)))
 
-  (deftest test-delete-get-put-get-delete-text
-    (let [key (str prefix "/" (uuid))
-          val (uuid)]
-      (s3/delete-key creds bucket key)
-      (is (thrown? Exception (s3/get-key-text creds bucket key)))
-      (s3/put-key-text creds bucket key val)
-      (is (= val (s3/get-key-text creds bucket key)))
-      (s3/delete-key creds bucket key)))
+  (deftest test-mk-key
+    (is (= "testing/cljs_aws/123"  (mk-key "/123")))
+    (is (= "testing/cljs_aws/123"  (mk-key "123")))
+    (is (= "testing/cljs_aws/123"  (mk-key "123")))
+    (is (= "testing/cljs_aws/123"  (mk-key "/123"))))
 
-  (deftest test-delete-get-put-get-delete-path
-    (let [key (str prefix "/" (uuid))
-          val (uuid)
-          path (temp-path)]
-      (s3/delete-key creds bucket key)
-      (is (thrown? Exception (s3/get-key-text creds bucket key)))
-      (spit path val)
-      (s3/put-key-path creds bucket key path)
-      (is (= val (s3/get-key-text creds bucket key)))
-      (s3/delete-key creds bucket key))))
+  (deftest test-put-get-text
+    (with-wipe-test-keys
+      (let [key (mk-key (uuid))
+            val (uuid)]
+        (s3/put-key-text creds bucket key val)
+        (is (= val (s3/get-key-text creds bucket key))))))
+
+  (deftest test-put-get-path
+    (with-wipe-test-keys
+      (let [key (mk-key (uuid))
+            val (uuid)
+            path (temp-path)]
+        (spit path val)
+        (s3/put-key-path creds bucket key path)
+        (is (= val (s3/get-key-text creds bucket key))))))
+
+  (deftest test-list
+    (with-wipe-test-keys
+      (mapv #(s3/put-key-text creds bucket (mk-key %) "")
+            ["a.txt"
+             "dir1/b.txt"])
+      (testing "normal list shows the contents of the prefix directory's keys and dirs"
+        (is (= (->> ["dir1/"
+                     "a.txt"]
+                    (mapv mk-key))
+               (s3/list-all creds bucket prefix))))
+      (testing "normal list :dirs-only the contents of the prefix directory's dirs"
+        (is (= (->> ["dir1/"]
+                    (mapv mk-key))
+               (s3/list-all creds bucket prefix :dirs-only true))))
+      (testing "normal list :dirs-only the contents of the prefix directory's dirs"
+        (is (= (->> ["a.txt"]
+                    (mapv mk-key))
+               (s3/list-all creds bucket prefix :keys-only true))))
+      (testing "normal list :recursive shows all keys for the current prefix"
+        (is (= (->> ["a.txt"
+                     "dir1/b.txt"]
+                    (mapv mk-key))
+               (s3/list-all creds bucket prefix :recursive true)))))))
