58b9a90b3a4e88a196b1b4dfc7a9fd299a94ac1d
nathants <me@nathants.com>
2015-12-12 21:26:10 -0800

    refactor

--- src/aws/s3.clj
+++ src/aws/s3.clj
@@ -22,6 +22,8 @@
   (->> [0]
     (when (not (number? (last flags))))
     (concat flags)
+    (map str)
+    (map #(s/replace % #"/$" ""))
     (apply str)
     clojure.lang.Murmur3/hashUnencodedChars
     (str (-cache-dir) "/s3cache_")))
@@ -29,10 +31,10 @@
 (defn -cached-get-key-stream
   "Creates a disk cached get-key-stream fn."
   [get-key-stream]
-  (fn [creds bucket key]
-    (let [path (-cache-path bucket key)]
+  (fn [creds bucket k]
+    (let [path (-cache-path bucket k)]
       (when-not (-> path java.io.File. .exists)
-        (with-open [stream (get-key-stream creds bucket key)
+        (with-open [stream (get-key-stream creds bucket k)
                     reader (io/reader stream)]
           (->> reader line-seq (s/join "\n") (spit path))))
       (io/reader path))))
@@ -41,17 +43,11 @@
   "Creates a lazy disk cached list-keys fn. Please note that because this is lazy,
    the cache will only contain as many keys as the first caller consumed. "
   [list-keys]
-  (let [versions (fn [prefix]
-                   [(s/replace prefix #"/$" "")
-                    (str (s/replace prefix #"/$" "") "/")])
-        lazy-read-cache (fn [bucket prefix]
-                          (->> (versions prefix)
-                            (map (fn [p]
-                                   (->> (map #(-cache-path bucket p %) (range))
-                                     (take-while #(-> % java.io.File. .exists))
-                                     (map slurp)
-                                     (map #(s/split % #"\n"))
-                                     (apply concat))))
+  (let [lazy-read-cache (fn [bucket prefix]
+                          (->> (map #(-cache-path bucket prefix %) (range))
+                            (take-while #(-> % java.io.File. .exists))
+                            (map slurp)
+                            (map #(s/split % #"\n"))
                             (apply concat)
                             sort))
         lazy-generate-cache (fn [creds bucket prefix marker]
@@ -64,9 +60,7 @@
                                  (partition-all *max-keys*)
                                  (map vector (range)))))
         has-cache (fn [bucket prefix]
-                    (->> (versions prefix)
-                      (map #(-> (-cache-path bucket %) java.io.File. .exists))
-                      (some identity)))]
+                    (-> (-cache-path bucket prefix) java.io.File. .exists))]
     (fn [creds bucket prefix & [marker]]
       (if (has-cache bucket prefix)
         (lazy-read-cache bucket prefix)
@@ -74,8 +68,8 @@
 
 (defn -prefixes
   "For a given key, find all the prefixes that would return that key."
-  [key]
-  (as-> key $
+  [k]
+  (as-> k $
     (s/split $ #"/")
     (butlast $)
     (map #(take % $) (->> $ count range (map inc)))
@@ -83,12 +77,12 @@
 
 (defn -indexed-keys
   "Create a mapping of prefix->keys for every prefix of every key provided."
-  [keys]
-  (let [conj-key (fn [key acc prefix]
-                   (update-in acc [prefix] #(conj (or % []) key)))
-        build-index (fn [acc key]
-                      (reduce (partial conj-key key) acc (-prefixes key)))]
-    (reduce build-index {} keys)))
+  [ks]
+  (let [conj-key (fn [k acc prefix]
+                   (update-in acc [prefix] #(conj (or % []) k)))
+        build-index (fn [acc k]
+                      (reduce (partial conj-key k) acc (-prefixes k)))]
+    (reduce build-index {} ks)))
 
 (defn -stub-s3
   "Take data, a mapping of {bucket {key str}}, and create cache files for
@@ -96,13 +90,13 @@
    already been fetched and cached. Returns the paths of the cache files created."
   [data]
   (doseq [[bucket keys->contents] data]
-    ;; stub all possible prefixes for all keys
+    ;; stub all possible prefixes for all ks
     (doseq [[prefix ks] (-> keys->contents keys -indexed-keys)]
       (doseq [[i ks] (->> ks (partition-all *max-keys*) (map vector (range)))]
         (spit (-cache-path bucket prefix i) (str (s/join "\n" ks) "\n") :append true)))
     ;; stub keys contents
-    (doseq [[key content] keys->contents]
-      (spit (-cache-path bucket key) content))))
+    (doseq [[k content] keys->contents]
+      (spit (-cache-path bucket k) content))))
 
 (defn list-all
   "Returns a lazy-seq of keys and/or prefixes for a given bucket and prefix.
@@ -133,43 +127,43 @@
 
 (defn get-key-stream
   "Get key as InputStream"
-  [creds bucket key]
-  (:input-stream (amz.s3/get-object creds bucket key)))
+  [creds bucket k]
+  (:input-stream (amz.s3/get-object creds bucket k)))
 
 (defn get-key-str
-  "Get key as str."
-  [creds bucket key]
-  (slurp (get-key-stream creds bucket key)))
+  "Get k as str."
+  [creds bucket k]
+  (slurp (get-key-stream creds bucket k)))
 
 (defn get-key-path
-  "Download key to path."
-  [creds bucket key path]
-  (with-open [r (get-key-stream creds bucket key)]
+  "Download k to path."
+  [creds bucket k path]
+  (with-open [r (get-key-stream creds bucket k)]
     (io/copy r (io/file path))))
 
 (defn put-key-str
   "Upload str to key."
-  [creds bucket key str]
+  [creds bucket k str]
   (let [bytes (.getBytes str "UTF-8")
         stream (java.io.ByteArrayInputStream. bytes)
         metadata {:content-length (count bytes)}]
-    (amz.s3/put-object creds :bucket-name bucket :key key :input-stream stream :metadata metadata)))
+    (amz.s3/put-object creds :bucket-name bucket :key k :input-stream stream :metadata metadata)))
 
 (defn put-key-path
   "Upload the file at path to key."
-  [creds bucket key path]
-  (amz.s3/put-object creds :bucket-name bucket :key key :file path))
+  [creds bucket k path]
+  (amz.s3/put-object creds :bucket-name bucket :key k :file path))
 
 (defn delete-key
   "Delete key."
-  [creds bucket key]
-  (amz.s3/delete-object creds bucket key))
+  [creds bucket k]
+  (amz.s3/delete-object creds bucket k))
 
 (defn delete-keys
   "Delete multiple keys in a single request."
-  [creds bucket keys]
-  (if (-> keys count (> 0))
-    (amz.s3/delete-objects creds :bucket-name bucket :keys (mapv #(DeleteObjectsRequest$KeyVersion. %) keys))))
+  [creds bucket ks]
+  (if (-> ks count (> 0))
+    (amz.s3/delete-objects creds :bucket-name bucket :keys (mapv #(DeleteObjectsRequest$KeyVersion. %) ks))))
 
 (defmacro with-cached-s3
   "Within this form, all calls to list-keys and get-key-* will be cached to disk, and only hit the network the first time.
