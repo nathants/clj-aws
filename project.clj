62cf92cde1c8adf861447e6d5c48531abc9a408c
nathants <me@nathants.com>
2016-04-06 23:25:45 -0700

    bump deps

--- project.clj
+++ project.clj
@@ -3,6 +3,5 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [amazonica "0.3.33"]
-                 [cheshire "5.5.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [amazonica "0.3.52"]])
